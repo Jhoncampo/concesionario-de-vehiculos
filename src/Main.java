61d1e33398d5e9ddbf1104cce5286c06ec008aef
Jhon <eduardcamayo13@gmail.com>
2026-05-03 10:56:57 -0500

    Implement dealership menu for vehicle management and sales registration

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,124 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import java.util.Scanner;
+
 import model.Car;
 import model.Customer;
+import model.Motorcycle;
 import model.Sale;
+import model.Truck;
 import model.Vehicle;
+import service.SaleService;
+import service.VehicleService;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This a car");
+        Scanner scanner = new Scanner(System.in);
+        VehicleService vehicleService = new VehicleService();
+        SaleService saleService = new SaleService();
 
-        Vehicle car = new Car("100","Toyota", 2026, 0);
-        Vehicle truck = new Car("200","Mercedez", 2020, 15000);
-        Vehicle motorcycle = new Car("300","Duke", 2025, 1000);
-        Customer client1 = new Customer("Jhon", "Campo", "1067461226");
-        Sale sale1 = new Sale(1, motorcycle, client1);
-     
-        // System.out.println(car);
-        // System.out.println(truck);
-        // System.out.println(motorcycle);
-        // System.out.println(client1);
-        System.out.println(sale1);
+        int option;
+
+        do {
+            System.out.println("\n DEALERSHIP MENU");
+            System.out.println("1. Add Vehicle");
+            System.out.println("2. Show Vehicles");
+            System.out.println("3. Register Sale");
+            System.out.println("4. Show Sales");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    System.out.println("\nSelect vehicle type:");
+                    System.out.println("1. Car");
+                    System.out.println("2. Truck");
+                    System.out.println("3. Motorcycle");
+                    int typeOption = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Code: ");
+                    String code = scanner.nextLine();
+
+                    System.out.print("Brand: ");
+                    String brand = scanner.nextLine();
+
+                    System.out.print("Year: ");
+                    int year = scanner.nextInt();
+
+                    System.out.print("Mileage: ");
+                    double mileage = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    Vehicle vehicle = null;
+
+                    switch (typeOption) {
+                        case 1:
+                            vehicle = new Car(code, brand, year, mileage);
+                            break;
+                        case 2:
+                            vehicle = new Truck(code, brand, year, mileage);
+                            break;
+                        case 3:
+                            vehicle = new Motorcycle(code, brand, year, mileage);
+                            break;
+                        default:
+                            System.out.println("Invalid type");
+                    }
+
+                    if (vehicle != null) {
+                        vehicleService.create(vehicle);
+                        System.out.println("Vehicle added successfully");
+                    }
+                    break;
+                case 2:
+                    System.out.println("\n=== VEHICLES ===");
+                    vehicleService.findAll().forEach(System.out::println);
+                    break;
+                case 3:
+                    System.out.print("\nEnter vehicle code: ");
+                    String vehicleCode = scanner.nextLine();
+
+                    Vehicle foundVehicle = vehicleService.findByCode(vehicleCode);
+
+                    if (foundVehicle == null) {
+                        System.out.println("Vehicle not found");
+                        break;
+                    }
+                    System.out.print("Customer First Name: ");
+                    String firstName = scanner.nextLine();
+
+                    System.out.print("Customer Last Name: ");
+                    String lastName = scanner.nextLine();
+
+                    System.out.print("Customer Document: ");
+                    String document = scanner.nextLine();
+
+                    System.out.print("Sale Amount: ");
+                    double amount = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    Customer customer = new Customer(firstName, lastName, document);
+                    Sale sale = new Sale(amount, foundVehicle, customer);
+
+                    saleService.create(sale);
+                    System.out.println("Sale registered successfully");
+                    break;
+                case 4:
+                    System.out.println("\n=== SALES ===");
+                    saleService.findAll().forEach(System.out::println);
+                    break;
+                case 5:
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        } while (option != 5);
+        scanner.close();
     }
 }
