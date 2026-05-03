1bb14c1e26ad21d94d9e656fd46f9635bcf9498a
Jhon <eduardcamayo13@gmail.com>
2026-05-02 21:11:19 -0500

    Add DataStorage class to manage vehicle and sale lists

---
+++ src/repository/DataStorage.java
@@ -0,0 +1,12 @@
+package repository;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import model.Sale;
+import model.Vehicle;
+
+public class DataStorage{
+    public static List<Vehicle> vehicles = new LinkedList<>();
+    public static List<Sale> sales = new LinkedList<>();
+}
